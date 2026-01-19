e049a908fc7907ba8e2b557b094e49bc26dd9392
Cary <cary0507.zhuang@gmail.com>
2026-01-19 15:16:25 -0500

    All components/pages created. Routers between pages have been set.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { EventDetails } from './event-details/event-details';
+import { CalendarView } from './calendar-view/calendar-view';
+import { Notifications } from './notifications/notifications';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: Home},
+    {path: 'event-details', component: EventDetails},
+    {path: 'calendar-view', component: CalendarView},
+    {path: 'notifications', component: Notifications}
+];
