49e8e081e8965bfb687908d3c165e05368a76a20
“artinmehri” <artinmehri2023@gmail.com>
2026-03-18 02:55:53 -0400

    added landing page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { EventDetails } from './event-details/event-details';
 import { CalendarView } from './calendar-view/calendar-view';
 import { Notifications } from './notifications/notifications';
 import { Waitlist } from './waitlist/waitlist';
+import { LandingPage } from './landing-page/landing-page';
 
 export const routes: Routes = [
     {path: '', component: Waitlist},
     {path: 'home', component: Home},
     {path: 'home/:event', component: EventDetails},
     {path: 'calendar-view', component: CalendarView},
-    {path: 'notifications', component: Notifications}
+    {path: 'notifications', component: Notifications},
+    {path: 'about', component: LandingPage}
 ];
